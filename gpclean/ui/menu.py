6cbe2e62bdbfd0558b6bc8f7a597f8e54d01b51e
kangseyoung <ksey020501@g.hongik.ac.kr>
2025-06-25 19:46:30 +0900

    20251946

--- gpclean/ui/menu.py
+++ gpclean/ui/menu.py
@@ -20,20 +20,21 @@ if not logger.handlers:
 
 def create_menu():
     import maya.cmds as cmds
-    import maya.mel as mel
     try:
         from gpclean.main import launch_login_ui
+        if cmds.menu("PRFS", exists=True):
+            cmds.deleteUI("PRFS", menu=True)
+            logger.info("기존 메뉴 PRFS 삭제함")
 
-        menu_bar = mel.eval('$tmp = $gMainWindowMenuBar')
-
-        if cmds.menu('PRFS', exists=True):
-            cmds.deleteUI('PRFS', menu=True)
-            logger.info("기존 PRFS 메뉴 삭제")
-
-        custom_menu = cmds.menu('PRFS', parent=menu_bar, tearOff=True, label='PRFS')
-        cmds.menuItem(label="submit to deadline", parent=custom_menu, command=lambda *_: launch_login_ui())
+        # 부모를 'MayaWindow'로 하면 정상적으로 생성됨 (Python 방식)
+        cmds.setParent("MayaWindow")
+        cmds.menu("PRFS", label="PRFS", tearOff=True)
+        cmds.menuItem(label="Submit to Deadline", parent="PRFS", command=lambda *_: launch_login_ui())
 
         logger.info("PRFS 메뉴 생성 완료")
+
+    except Exception as e:
+        logger.exception("메뉴 생성 중 에러 발생: %s", e)
 
     except Exception as e:
         logger.exception(e)
