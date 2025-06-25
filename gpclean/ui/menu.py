98364bc5440ba7f16916256a7c72154c80fbd180
kangseyoung <ksey020501@g.hongik.ac.kr>
2025-06-25 19:09:52 +0900

    202519009

--- gpclean/ui/menu.py
+++ gpclean/ui/menu.py
@@ -15,17 +15,21 @@ if not logger.handlers:
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
-# 메뉴 생성 함수
 def create_menu():
+    import maya.cmds as cmds
+    import maya.mel as mel
+
     try:
-        import maya.cmds as cmds
         from gpclean.main import launch_login_ui
+
+        # 메뉴 바 layout 이름 가져오기
+        main_menu = mel.eval('$tmp=$gMainWindowMenuBar')
 
         if cmds.menu("GPCleanMenu", exists=True):
             cmds.deleteUI("GPCleanMenu", menu=True)
             logger.info("기존 GPCleanMenu 삭제함")
 
-        cmds.menu("GPCleanMenu", label="PRFS", parent="MayaWindow", tearOff=True)
+        cmds.menu("GPCleanMenu", label="PRFS", parent=main_menu, tearOff=True)
         cmds.menuItem(label="submit to deadline", parent="GPCleanMenu", command=lambda *_: launch_login_ui())
 
         logger.info("GPCleanMenu 생성 완료")
