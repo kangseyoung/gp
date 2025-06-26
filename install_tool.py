02a6ffc8a6601e89bac751d5c97277d74109922f
kangseyoung <ksey020501@g.hongik.ac.kr>
2025-06-27 00:53:55 +0900

    20250627 install tool.py edit

--- install_tool.py
+++ install_tool.py
@@ -8,18 +8,19 @@ maya_scripts_dir = Path.home() / "OneDrive"/ "문서" / "maya" / "2023" /"script
 site_packages_dir = maya_scripts_dir / "site-packages"
 print(maya_scripts_dir)
 # 1. 라이브러리 설치
+
 subprocess.run([
     sys.executable,
-    "-m", "pip", "install", "-r", "requirements.txt",
+    "-m", "pip", "install", "-r", "D:/gitclonetest/gp/requirements.txt",
     "--target", str(site_packages_dir)
 ])
 
-source_dir = "D:\gitclonetest\gp\gpclean"
+source_dir = "D:/gitclonetest/gp/gpclean"
 # 2. 툴 소스코드 복사
 import shutil
 shutil.copytree(source_dir, maya_scripts_dir / "gpclean", dirs_exist_ok=True)
 
 # 3. userSetup.py 덮어쓰기
-shutil.copy("userSetup.py", maya_scripts_dir / "userSetup.py")
+shutil.copy("D:/gitclonetest/gp/userSetup.py", maya_scripts_dir / "userSetup.py")
 
 print("[+] 설치 완료! 마야를 실행하면 gpclean 이 자동 로드됩니다.")
